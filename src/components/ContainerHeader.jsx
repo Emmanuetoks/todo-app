f1c3ca479ebb917d7ed0f0d5647bb7201cad1d3d
ArmyofDeux <103246856+Emmanuetoks@users.noreply.github.com>
2023-05-08 12:29:16 -0700

    Finsihed the Styling

--- src/components/ContainerHeader.jsx
+++ src/components/ContainerHeader.jsx
@@ -2,16 +2,16 @@ const ContainerHeader = () => {
   const changeTheme = () => {
     const body = document.querySelector('body');
     const currentTheme = body.className;
-
     if (currentTheme === 'theme-1') {
       body.className = 'theme-2';
     } else if (currentTheme === 'theme-2') {
       body.className = 'theme-1';
     }
+    
   }
   return (
     <header className="container__header flex">
-      <h1 className="uppercase container__logo">todo</h1>
+      <h1 className="uppercase container__logo fw-700">todo</h1>
       <button onClick={changeTheme} className="container__theme-btn">
         <span className="sr-only">Theme-toggler</span>
       </button>
--- src/components/TodoBox.jsx
+++ src/components/TodoBox.jsx
@@ -1,47 +1,76 @@
+const CheckBox = () => {
+  const makeCheckBackground = (e) => {
+    e.target.classList.toggle('checkbox--checked');
+    const todoItem = e.target.nextSibling;
+
+    if (todoItem.id != 'inputBox') {
+      if (todoItem.classList.contains('completed')) {
+        todoItem.classList.remove('completed')
+      } else {
+        todoItem.classList.add('completed')
+      }
+    }
+  }
+  return (
+    <div onClick={makeCheckBackground} className="todo__checkbox pos-rel">
+      <span className="sr-only">checkbox</span>
+    </div>
+  )
+}
+
+const TodoItem = ({ todoName }) => {
+  return (
+    <div className={`todo__item fs-200 flex`} style={{ '--gap': '1rem' }}>
+      <CheckBox />
+      <p>{todoName}</p>
+
+      <button className="todo__delete-button">
+        <span className="sr-only">Delete Button</span>
+      </button>
+    </div>
+  )
+}
+
 const TodoBox = () => {
   return (
     <div className="todo flex" style={{ '--gap': '.5rem' }}>
-      <div className="todo__item">
-        <input type="text" className="add-todo fs-200" placeholder="Create todo item" />
+      <div className="todo__item flex">
+        <CheckBox />
+        <input id="inputBox" type="text" className="add-todo fs-200" placeholder="Create todo item" />
       </div>
 
-      <div className="todo__list fs-200">
-        <div className="todo__item">
-          <div className="todo__checkbox">
-            <span className="sr-only">checkbox</span>
-          </div>
-          <p>
-            dummy todo
+      <div className="todo__list flex-col border-radius-5px" style={{ '--gap': '1px' }}>
+        <TodoItem todoName={'type hello world'} />
+
+        <footer className="todo__footer flex pos-rel">
+          <p className="todo__items-left">
+            <span className="todo-option">x-mins-left</span>
           </p>
-        </div>
+
+          <div className="todo__options fw-400 border-radius-5px flex" style={{ '--gap': '1rem' }}>
+            <span role="button" className="todo__option">
+              All
+            </span>
+
+            <span role="button" className="todo__option">
+              Active
+            </span>
+
+            <span role="button" className="todo__option">
+              Completed
+            </span>
+          </div>
+
+
+          <p>
+            <span className="todo__option">
+              Clear Completed
+            </span>
+          </p>
+        </footer>
       </div>
 
-      <footer className="todo__footer flex pos-rel">
-        <p className="todo__items-left">
-          <span className="todo-option">x-mins-left</span>
-        </p>
 
-        <div className="todo__options flex" style={{ '--gap': '1rem' }}>
-          <span role="button" className="todo__option">
-            All
-          </span>
-
-          <span role="button" className="todo__option">
-            Active
-          </span>
-
-          <span role="button" className="todo__option">
-            Completed
-          </span>
-        </div>
-
-
-        <p>
-          <span className="todo__option">
-            clear completed
-          </span>
-        </p>
-      </footer>
     </div>
   )
 }
